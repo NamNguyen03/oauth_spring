b5ccb37244efc7eb71f729bc13051ba1263770aa
Nam Nguyen <nam030319991@gmail.com>
2021-01-09 01:21:47 +0700

    Spring OAuth2

--- resouceServer/src/main/java/com/namNguyen03/resouceServer/config/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.namNguyen03.resouceServer.config;
-
-public class Constants {
-	public static final String JPA_UNIT_NAME ="PERSITENCE_UNIT_NAME";
-	public static final String PACKAGE_ENTITIES = "com.namNguyen03.resouceServer.model";
-}
--- resouceServer/src/main/java/com/namNguyen03/resouceServer/service/OAuth2Service.java
+++
@@ -1,7 +0,0 @@
-package com.namNguyen03.resouceServer.service;
-
-import com.namNguyen03.resouceServer.model.AccessToken;
-
-public interface OAuth2Service {
-    AccessToken getAccessToken(String clientId, String clientSecret, String redirectUri, String code, String state);
-}
