7fc920b1dbd2196616f668673fa6ccc5a28b0df1
Nam Nguyen <nam030319991@gmail.com>
2021-01-13 17:06:10 +0700

    add comment

--- authorizationServer/src/main/java/com/namNguyen03/authorizationServer/AuthorizationServerApplication.java
+++ authorizationServer/src/main/java/com/namNguyen03/authorizationServer/AuthorizationServerApplication.java
@@ -2,6 +2,12 @@ package com.namNguyen03.authorizationServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+create by: 	 Nguyen Duc Nam
+email: 		 nam03031999@gmail.com
+link github: https://github.com/NamNguyen03
+create date: 01/08/2020 (mm/dd/yyyy) 
+*/
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
